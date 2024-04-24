ef1d1642d7792adb5a06cb8a794303899af70894
fsigo <eljahmed40@gmail.com>
2024-04-25 00:30:23 +0100

    replay ability fix-up

--- src/ClientSide.java
+++ src/ClientSide.java
@@ -4,24 +4,54 @@ import java.io.*;
 public class ClientSide {
 
     public static void main(String[] args) {
-        try{
-            System.out.println("Client Side start ");
-            Socket soc = new Socket("localhost",3000);
+        try {
+            Socket soc = new Socket("localhost", 3000);
+            System.out.println("[CLIENT]: Client Side started on port 3000 ");
+
+            BufferedReader in = new BufferedReader(new InputStreamReader(soc.getInputStream()));
+            PrintWriter out = new PrintWriter(soc.getOutputStream(), true);
+            BufferedReader userInput = new BufferedReader(new InputStreamReader(System.in));
+
+            String serverMessage = in.readLine();
+            System.out.println(serverMessage);
+
             while (true) {
-            BufferedReader userInput = new BufferedReader (new InputStreamReader(System.in));
-            System.out.println("Enter a number between 0 and 20 :");
-            String str = userInput.readLine();
-            if (Integer.parseInt(str) > 20 || Integer.parseInt(str) < 0) {
-                System.out.println("Invalid number: please enter a number between 0 and 20");
-                continue;
+                String str = userInput.readLine();
+                try {
+                    int x = Integer.parseInt(str); //check if the input is a number
+                } catch (NumberFormatException e) {
+                    System.out.println("[CLIENT]: Must enter a Number between 0 and 20");
+                    continue;
+                }
+                if (Integer.parseInt(str) > 20 || Integer.parseInt(str) < 0) {
+                    System.out.println("[CLIENT]: Invalid number, only a number between 0 and 20");
+                    continue;
+                }
+                out.println(str);
+
+                serverMessage = in.readLine();
+                System.out.println(serverMessage);
+
+                if (serverMessage.contains("[SERVER]: You won!")) {
+                    String userReplay = userInput.readLine();
+                    out.println(userReplay);
+                    if (userReplay.equalsIgnoreCase("yes")) {
+                        System.out.println("[CLIENT]: sending replay request to the server.");
+                        System.out.println(in.readLine());
+                        continue;
+                    } else {
+                        serverMessage = in.readLine();
+                        System.out.println(serverMessage);
+                        System.out.println("[CLIENT]: Exiting the game.");
+                        break;
+                    }
+                }
             }
-            PrintWriter out = new PrintWriter(soc.getOutputStream(),true);
-            out.println(str);
-            BufferedReader in = new BufferedReader (new InputStreamReader(soc.getInputStream()));
-            System.out.println(in.readLine());
-            }
-}catch(Exception e){
-    e.printStackTrace();
-}
+
+            soc.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
     }
 }
--- src/ServerSide.java
+++ src/ServerSide.java
@@ -2,43 +2,58 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.io.*;
 import java.util.Random;
-import java.net.ServerSocket;
-import java.net.Socket;
-import java.io.*;
-import java.util.Random;
 
 public class ServerSide {
 
     public static void main(String[] args) {
         try {
-            System.out.println("Server waiting for user to give a number");
             ServerSocket ss = new ServerSocket(3000);
+            System.out.println("[SERVER]: Server started. Waiting for Client response...");
             Socket soc = ss.accept();
-            System.out.println("Connection Successful");
+            System.out.println("[SERVER]: Connection Successful");
+
+            Random random = new Random();
+            int randomNumber = random.nextInt(21);
+            System.out.println("[SERVER]: number generated is " + randomNumber);
+
             BufferedReader in = new BufferedReader(new InputStreamReader(soc.getInputStream()));
             PrintWriter out = new PrintWriter(soc.getOutputStream(), true);
 
-            // Generate a random integer between 0 and 20
-            Random random = new Random();
-            int randomNumber = random.nextInt(21);
+            out.println("[SERVER]: Guess a number between 0 and 20.");
 
             while (true) {
-                String str = in.readLine();
-                if (str == null) {
-                    break;
+                String inputString = in.readLine();
+                if (inputString == null) {
+                    continue;
                 }
-                int x = Integer.parseInt(str);
-                System.out.println("Server received: " + str);
-                if (x == randomNumber) {
-                    out.println("Server says: You won");
-                    out.println("Random number generated: " + randomNumber);
-                    break;
-                } else if (x > randomNumber) {
-                    out.println("Server says: the number is lesser");
-                } else if (x < randomNumber) {
-                    out.println("Server says: the number is greater");
+                int guessedNumber = Integer.parseInt(inputString);
+                System.out.println("[SERVER]: received " + inputString);
+
+                if (guessedNumber == randomNumber) {
+                    System.out.println("[SERVER]: Client won, sending 'winning' message to client");
+                    out.println("[SERVER]: You won! replay? yes/no");
+
+                    System.out.println("[SERVER]: waiting for replay message...");
+                    String replayMessage = in.readLine();
+                    if (replayMessage != null && replayMessage.equalsIgnoreCase("yes")) {
+                        System.out.println("[SERVER]: Client accepted replay request");
+                        randomNumber = random.nextInt(21);
+                        System.out.println("[SERVER]: New number generated is " + randomNumber);
+                        out.println("[SERVER]: New game started. Guess a number between 0 and 20.");
+                        continue;
+                    } else {
+                        out.println("[SERVER]: Server shutting down...");
+                        System.out.println("[SERVER]: Client Refused to replay, shutting down...");
+                        break;
+                    }
+                } else if (guessedNumber > randomNumber) {
+                    out.println("[SERVER]: The number is lesser than " + guessedNumber);
+                } else if (guessedNumber < randomNumber) {
+                    out.println("[SERVER]: The number is greater than " + guessedNumber);
                 }
+                // soc.close();
             }
+
             soc.close();
             ss.close();
         } catch (Exception e) {
