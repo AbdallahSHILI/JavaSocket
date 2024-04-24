d3cb940ffaa1a014be9622689c256b27773a8458
fsigo <eljahmed40@gmail.com>
2024-04-24 12:35:42 +0100

    random guesser

--- src/ClientSide.java
+++ src/ClientSide.java
@@ -7,13 +7,19 @@ public class ClientSide {
         try{
             System.out.println("Client Side start ");
             Socket soc = new Socket("localhost",3000);
+            while (true) {
             BufferedReader userInput = new BufferedReader (new InputStreamReader(System.in));
             System.out.println("Enter a number between 0 and 20 :");
             String str = userInput.readLine();
+            if (Integer.parseInt(str) > 20 || Integer.parseInt(str) < 0) {
+                System.out.println("Invalid number: please enter a number between 0 and 20");
+                continue;
+            }
             PrintWriter out = new PrintWriter(soc.getOutputStream(),true);
             out.println(str);
             BufferedReader in = new BufferedReader (new InputStreamReader(soc.getInputStream()));
             System.out.println(in.readLine());
+            }
 }catch(Exception e){
     e.printStackTrace();
 }
--- src/ServerSide.java
+++ src/ServerSide.java
@@ -2,32 +2,45 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.io.*;
 import java.util.Random;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.io.*;
+import java.util.Random;
 
 public class ServerSide {
 
     public static void main(String[] args) {
         try {
-            System.out.println("Server waiting user to give a number ");
+            System.out.println("Server waiting for user to give a number");
             ServerSocket ss = new ServerSocket(3000);
             Socket soc = ss.accept();
             System.out.println("Connection Successful");
             BufferedReader in = new BufferedReader(new InputStreamReader(soc.getInputStream()));
-            String str = in.readLine();
-            
+            PrintWriter out = new PrintWriter(soc.getOutputStream(), true);
+
             // Generate a random integer between 0 and 20
             Random random = new Random();
             int randomNumber = random.nextInt(21);
-            PrintWriter out = new PrintWriter(soc.getOutputStream(), true);
-            out.println("Server says: " + str);
-            out.println("Random number generated: " + randomNumber);
-            if (Integer.parseInt(str) == randomNumber) {
-                out.println("Server says: " + "You won");
-            } else if (Integer.parseInt(str) > randomNumber) {
-                out.println("Server says: the number is greater");
-            } else {
-                out.println("Server says: the number is lesser");
-            };
 
+            while (true) {
+                String str = in.readLine();
+                if (str == null) {
+                    break;
+                }
+                int x = Integer.parseInt(str);
+                System.out.println("Server received: " + str);
+                if (x == randomNumber) {
+                    out.println("Server says: You won");
+                    out.println("Random number generated: " + randomNumber);
+                    break;
+                } else if (x > randomNumber) {
+                    out.println("Server says: the number is lesser");
+                } else if (x < randomNumber) {
+                    out.println("Server says: the number is greater");
+                }
+            }
+            soc.close();
+            ss.close();
         } catch (Exception e) {
             e.printStackTrace();
         }
